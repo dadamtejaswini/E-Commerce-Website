e219f8f47ebcc025064e64dff2544612a6cba0ca
dadamtejaswini <dadamtejaswini@gmail.com>
2026-01-12 22:26:36 +0530

    day-3 uploaded

---
+++ day-3/inventory-service/src/main/java/com/E_Commerce/inventory_service/dto/InventoryResponse.java
@@ -0,0 +1,16 @@
+package com.E_Commerce.inventory_service.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryResponse {
+    private String code;
+    private Boolean inStock;
+}
