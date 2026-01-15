15c1789d217b3b4d65beb46f9c79cf4bd4e99c8f
dadamtejaswini <dadamtejaswini@gmail.com>
2026-01-15 23:33:40 +0530

    Delete product-service directory

--- product-service/src/main/java/com/website/product_service/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.website.product_service.repository;
-
-import com.website.product_service.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-}
