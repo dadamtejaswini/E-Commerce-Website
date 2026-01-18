023b90002fd41f602065c6aa359be58351327684
dadamtejaswini <dadamtejaswini@gmail.com>
2026-01-18 23:11:18 +0530

    updated

---
+++ day-7/api-gateway/src/main/java/com/E_Commerce/api_gateway/config/GatewayConfig.java
@@ -0,0 +1,32 @@
+package com.E_Commerce.api_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("product-service", r -> r
+                        .path("/api/product/**")
+                        .uri("lb://PRODUCT-SERVICE"))
+                .route("order-service", r -> r
+                        .path("/api/order/**")
+                        .uri("lb://ORDER-SERVICE"))
+                .route("eureka-web", r -> r
+                        .path("/eureka/web/**")
+                        .filters(f -> f.setPath("/"))
+                        .uri("http://localhost:8761"))
+
+                .route("eureka-api", r -> r
+                        .path("/eureka/**")
+                        .uri("http://localhost:8761"))
+                .build();
+    }
+
+}
